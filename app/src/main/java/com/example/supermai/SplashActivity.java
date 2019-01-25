23c44d6c8f23a32c602add48463f2a000c7731e6
CarolJim <cjimenez@pagatodo.com>
2019-01-25 15:07:58 -0600

    primera pantalla

---
+++ app/src/main/java/com/example/supermai/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.supermai;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends Activity {
+
+    private Timer timer;
+    private ProgressBar progressBar;
+    private int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setProgress(0);
+        final long intervalo = 45;
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (i < 100){
+                    progressBar.setProgress(i);
+                    i++;
+                }else{
+                    timer.cancel();
+                    Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, 0, intervalo);
+    }
+}
